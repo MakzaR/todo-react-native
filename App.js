ffd52f6ddee000418c5a5b216a5d9610a26ca578
MakzaR <makzarchukov@gmail.com>
2021-09-25 12:00:41 +0500

    added flex

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import {StatusBar} from 'expo-status-bar';
 import React, {useState} from 'react';
-import {StyleSheet, Text, View, FlatList} from 'react-native';
+import {StyleSheet, Text, View, FlatList, TouchableWithoutFeedback, Keyboard} from 'react-native';
 
 import Header from "./components/Header";
 import TodoItem from "./components/TodoItem";
@@ -17,29 +17,31 @@ export default function App() {
 
     const addHandler = (text) => {
         setTodos((prevTodos) => {
-           return [
-               {text: text, key: Math.random().toString()},
-               ...prevTodos
-           ]
+            return [
+                {text: text, key: Math.random().toString()},
+                ...prevTodos
+            ]
         });
     }
 
     return (
-        <View style={styles.container}>
-            <Header/>
-            <View style={styles.content}>
-                <AddFrom addHandler={addHandler}/>
-                <FlatList
-                    style={styles.list}
-                    data={todos}
-                    renderItem={({item}) => (
-                        <TodoItem
-                            item={item}
-                            pressHandler={pressHandler}
-                        />
-                    )}/>
+        <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+            <View style={styles.container}>
+                <Header/>
+                <View style={styles.content}>
+                    <AddFrom addHandler={addHandler}/>
+                    <FlatList
+                        style={styles.list}
+                        data={todos}
+                        renderItem={({item}) => (
+                            <TodoItem
+                                item={item}
+                                pressHandler={pressHandler}
+                            />
+                        )}/>
+                </View>
             </View>
-        </View>
+        </TouchableWithoutFeedback>
     );
 }
 
@@ -49,9 +51,11 @@ const styles = StyleSheet.create({
         backgroundColor: '#fff',
     },
     content: {
+        flex: 1,
         padding: 20,
     },
     list: {
+        flex: 1,
         marginTop: 20,
     },
 });
